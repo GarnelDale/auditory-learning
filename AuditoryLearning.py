520040950ebead5bb9cb35633dc189048698e6f2
GarnelDale <bobbyo375@gmail.com>
2023-06-29 23:26:24 -0400

    Nearly finished source code

--- AuditoryLearning.py
+++ AuditoryLearning.py
@@ -5,12 +5,12 @@ import arcade
 import random
 
 # Screen title and size
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 1200
 SCREEN_TITLE = "Auditory Learning"
 
 # Constants for sizing
-TARGET_RADIUS = 15
+TARGET_RADIUS = 18
 DEFENCE_RADIUS = 50
 
 # Constants for number of different targets
@@ -34,15 +34,19 @@ CHORD_FILES = [['resources\D7_A4_Piano.wav','resources\D7_AS4_Piano.wav','resour
 'resources\mi7_F4_Piano.wav','resources\mi7_FS4_Piano.wav','resources\mi7_G4_Piano.wav','resources\mi7_GS4_Piano.wav']]
 
 # Constant start points for the different chord types
-CHORD_X = [0,10,15,5,20]
-CHORD_Y = [5,15,10,0,20]
+CHORD_X = [SCREEN_WIDTH/4,SCREEN_WIDTH/4*3,0,SCREEN_WIDTH/2,SCREEN_WIDTH]
+CHORD_Y = [0,0,SCREEN_HEIGHT/4*3,SCREEN_HEIGHT,SCREEN_HEIGHT/5*4]
 
 # Constant for the color of the different types
-TYPE_COLOR = [arcade.color.AIR_FORCE_BLUE, arcade.color.ANDROID_GREEN, 
-arcade.color.BANANA_YELLOW, arcade.color.BRILLIANT_ROSE, arcade.color.CYBER_GRAPE]
+TYPE_COLOR = [(122, 40, 203), (165, 1, 4), 
+(176, 254, 118), (255, 190, 239), (68, 204, 255) ]
 
 # Constant for base rate of travel
 RATE_OF_TRAVEL = .5
+
+# Global variables for number of runs and current time
+run = 1
+timer = 30
 
 class Target(arcade.SpriteCircle):
     """ Base Target Class """
@@ -95,6 +99,8 @@ class MyGame(arcade.Window):
         self.defence_list = None
         self.score = 0
         self.sound = None
+        self.failure = 0
+        self.original_run = 1
 
         arcade.set_background_color(arcade.color.BLACK)
 
@@ -105,6 +111,10 @@ class MyGame(arcade.Window):
         self.target_list = []
         self.active_target_list = arcade.SpriteList()
         self.defence_list = arcade.SpriteList()
+
+        self.score = 0
+        self.failure = 2400
+        self.original_run = run
 
         # Create every target
         for chord in range(CHORD_TYPES):
@@ -126,6 +136,8 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """ Render the screen. """
+        global timer
+
         # Clear the screen
         self.clear()
 
@@ -136,6 +148,17 @@ class MyGame(arcade.Window):
         else:
             self.active_target_list.draw()
         self.defence_list.draw()
+
+        # render the timer
+        mins, secs = divmod(timer, 60)
+        t = '{:02d}:{:02d}'.format(mins, secs)
+        arcade.draw_text(t, SCREEN_WIDTH - 100, SCREEN_HEIGHT- 30)
+
+        if self.failure < TARGET_RADIUS + DEFENCE_RADIUS:
+            if timer > 0:
+                arcade.draw_text("Hit space to continue.", SCREEN_WIDTH/3, SCREEN_HEIGHT/3 * 2)
+            else:
+                arcade.draw_text("Thank you for your participation. Please e-mail me the trainingResults.txt files that were created.", SCREEN_WIDTH/3, SCREEN_HEIGHT/4 * 3)
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """ Called when the user presses a mouse button. """
@@ -154,28 +177,41 @@ class MyGame(arcade.Window):
             self.sound = arcade.load_sound(active.chord)
             self.active_target_list.append(active)
 
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE and self.failure < TARGET_RADIUS + DEFENCE_RADIUS:
+            self.setup()
+
+
     def on_update(self, delta):
-        # Make sure there is a target
-        if len(self.active_target_list) == 0:
-            active = self.target_list.pop(0)
-            self.sound = arcade.load_sound(active.chord)
-            self.active_target_list.append(active)
-        
-        # Make sure the target hasn't collided with the defence point
-        failure = arcade.get_distance_between_sprites(self.defence_list[0], self.active_target_list[0])
-        if (failure < TARGET_RADIUS + DEFENCE_RADIUS):
-            # write a print to file for the active_target_list[0] chord type 
-            # and the "score" to represent which iteration  
-            x = 0
-        else:
-            self.active_target_list[0].move_to_center(RATE_OF_TRAVEL + self.score / 50)
-            if self.active_target_list[0].sound_frequency % 30 == 0:
-                arcade.play_sound(self.sound)
-            if ((self.score % len(self.target_list)) == 0):
-                random.shuffle(self.target_list)
-            if self.score >= 30:
-                self.active_target_list[0].draw_delay += 1
-            self.active_target_list[0].sound_frequency += 1
+        global timer, run
+
+        if timer > 0  and run == self.original_run:
+            # Make sure there is a target
+            if len(self.active_target_list) == 0:
+                active = self.target_list.pop(0)
+                self.sound = arcade.load_sound(active.chord)
+                self.active_target_list.append(active)
+            
+            # Make sure the target hasn't collided with the defence point
+            self.failure = arcade.get_distance_between_sprites(self.defence_list[0], self.active_target_list[0])
+            if (self.failure < TARGET_RADIUS + DEFENCE_RADIUS):
+                # write a print to file for the active_target_list[0] chord type 
+                # and the "score" to represent which iteration
+                result = open(f'trainingResults{run}.txt', 'w')
+                result.write(f'Subject failed on round {self.score+1} on chord {self.active_target_list[0].chord}')
+                result.close()
+                run += 1 
+            else:
+                self.active_target_list[0].move_to_center(RATE_OF_TRAVEL + self.score / 50)
+                if self.active_target_list[0].sound_frequency % 30 == 0:
+                    arcade.play_sound(self.sound)
+                if self.active_target_list[0].sound_frequency % 60 == 0:
+                    timer -= 1
+                if ((self.score % len(self.target_list)) == 0):
+                    random.shuffle(self.target_list)
+                if self.score >= 30:
+                    self.active_target_list[0].draw_delay += 1
+                self.active_target_list[0].sound_frequency += 1
 
 def main():
     """ Main function """
